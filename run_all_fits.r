2b873e944708fd999998b36bc6ae47b0cac8936a
George Cantwell <gcant@umich.edu>
2023-08-01 17:23:40 -0600

    Synthetic data. Real data not yet published.

--- run_all_fits.r
+++ run_all_fits.r
@@ -1,9 +1,7 @@
 source("fitting_fns.r")
 set.seed(42)
 
-S = read.csv('data/Rehnquist.csv', header=FALSE)
-fit_all_models(S, "rehn")
-
-S = read.csv('data/Roberts.csv', header=FALSE)
-fit_all_models(S, "rob")
+# Run on synthetic data.  Real data yet to be published
+S = read.csv('data/synthetic.csv', header=FALSE)
+fit_all_models(S, "synth")
 
